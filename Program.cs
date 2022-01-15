8d425cb4b0f0ba81738d1bc1f274833e376c681c
Thayana Vilaça Fortunato <76934028+thayana2021js@users.noreply.github.com>
2022-01-15 11:45:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa
+
+        }
+       
+
+    }
+}
